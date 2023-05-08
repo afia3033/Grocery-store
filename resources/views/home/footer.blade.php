79832f2a929577d2354833429f0cb2c43352b7ba
afia3033 <125754028+afia3033@users.noreply.github.com>
2023-05-09 01:56:38 +0600

    change

---
+++ resources/views/home/footer.blade.php
@@ -0,0 +1,50 @@
+ <!-- footer start -->
+ <footer>
+         <div class="container">
+            <div class="row">
+               <div class="col-md-4">
+                   <div class="full">
+                      <div class="logo_footer">
+                        <h5>Showroom</h5>
+                      </div>
+                      <div class="information_f">
+                        <p><strong>ADDRESS:</strong> Banani,Dhaka</p>
+                        <p><strong>TELEPHONE:</strong> 0155484545556</p>
+                        <p><strong>EMAIL:</strong> amiruzzaman@gmail.com</p>
+                      </div>
+                   </div>
+               </div>
+               <div class="col-md-8">
+                  <div class="row">
+                  <div class="col-md-7">
+                     <div class="row">
+                        <div class="col-md-6">
+                     <div class="widget_menu">
+                        
+                        <ul>
+                           
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6">
+                     <div class="widget_menu">
+                        <
+                        <ul>
+                         
+                        </ul>
+                     </div>
+                  </div>
+                     </div>
+                
+                                 </div>
+                              </fieldset>
+                           </form>
+                        </div>
+                     </div>
+                  </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- footer end -->
